4beed06195811c731b57e25d282bfb159c1b18c9
DMNKX <duefate@gmail.com>
2026-04-02 16:21:40 +0900

    Move sheets

--- src/config.js
+++ src/config.js
@@ -87,7 +87,7 @@ export function resolveConfig() {
       12,
     spreadsheetId,
     sheetRange:
-      process.env.SHEET_RANGE ?? userConfig.sheetRange ?? "Sheet1!A:C",
+      process.env.SHEET_RANGE ?? userConfig.sheetRange ?? "시트1!B:D",
     serviceAccountJsonPath,
     /** CI에서만 주입하는 경우 */
     serviceAccountJsonRaw:
